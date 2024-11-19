2a9537975fafb44b232ef3a06638d32120bc7e5a
Jamaal Kerr <kerrjamaal994@gmail.com>
2024-11-18 22:54:28 -0500

    Added Javascript file and updated project files

---
+++ world.js
@@ -0,0 +1,58 @@
+// Wait for the DOM content to be fully loaded before executing the script
+console.log("Script loaded");
+document.addEventListener("DOMContentLoaded", function () {
+
+    // Get references to HTML elements
+    var lookupButton = document.getElementById("lookup");
+    var lookupCitiesButton = document.getElementById("lookupCities");
+    var result = document.getElementById("result");
+    var searchValue = document.getElementById("country");
+
+    // Declare the XMLHttpRequest object
+    var Req;
+
+    // Function to make an XMLHttpRequest based on the lookup type (country or cities)
+    function makeRequest(lookupType) {
+        // Initialize the XMLHttpRequest object
+        Req = new XMLHttpRequest();
+
+        // Construct the URL for the Ajax request, including the country and lookup parameters
+        var url = "http://localhost/comp2245-assignment5/world.php?country=" + encodeURIComponent(searchValue.value) + "&lookup=" + lookupType;
+
+        // Open a GET request to the specified URL
+        Req.open('GET', url);
+        // Send the Ajax request
+        Req.send();
+        // Set up a callback function to handle the response
+        Req.onreadystatechange = respond;
+    }
+
+    // Event listener for the "Lookup" button
+    lookupButton.addEventListener("click", function () {
+        // Make a request for country information
+        makeRequest("country");
+    });
+
+    // Event listener for the "Lookup Cities" button
+    if (lookupCitiesButton) {
+        lookupCitiesButton.addEventListener("click", function () {
+            // Make a request for city information
+            makeRequest("cities");
+        });
+    }
+
+    // Callback function to handle the Ajax response
+    function respond() {
+        // Check if the request has completed
+        if (Req.readyState === XMLHttpRequest.DONE) {
+            // Check if the server responded with a 200 status (OK)
+            if (Req.status === 200) {
+                // Update the content of the "result" div with the server's response
+                result.innerHTML = Req.responseText;
+            } else {
+                // Alert the user if there was an error
+                alert('Error');
+            }
+        }
+    }
+});
--- world.php
+++ world.php
@@ -1,17 +1,74 @@
 <?php
+// Database connection parameters
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
+// Establish a PDO database connection
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
 
+// Retrieve country and lookupType from the query parameters
+$country = $_GET['country'] ?? null;
+$lookupType = $_GET['lookup'] ?? null;
+
+// Check if the country parameter is provided
+if ($country === null) {
+    // If not, retrieve all countries
+    $stmt = $conn->query("SELECT * FROM countries");
+} else {
+    // If country parameter is provided, check the lookup type
+    if ($lookupType === "cities") {
+        // If lookup type is "cities," retrieve city information with a JOIN between cities and countries tables
+        $stmt = $conn->prepare("SELECT cities.name AS city_name, cities.district, cities.population
+                                FROM cities
+                                JOIN countries ON cities.country_code = countries.code
+                                WHERE countries.name = :country");
+    } else {
+        // If lookup type is not specified or is not "cities," retrieve country information
+        $stmt = $conn->prepare("SELECT * FROM countries WHERE name = :country");
+    }
+
+    // Bind the country parameter and execute the query
+    $stmt->bindParam(':country', $country);
+    $stmt->execute();
+}
+
+// Fetch the results as an associative array
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+
+<!-- HTML table to display the results -->
+<table>
+    <?php if ($lookupType === "cities"): ?>
+        <!-- If lookup type is "cities," display city information -->
+        <tr>
+            <th>City Name</th>
+            <th>District</th>
+            <th>Population</th>
+        </tr>
+        <?php foreach ($results as $row): ?>
+            <tr>
+                <td><?= $row['city_name']; ?></td>
+                <td><?= $row['district']; ?></td>
+                <td><?= $row['population']; ?></td>
+            </tr>
+        <?php endforeach; ?>
+    <?php else: ?>
+        <!-- If lookup type is not specified or is not "cities," display country information -->
+        <tr>
+            <th>Country Name</th>
+            <th>Continent</th>
+            <th>Independence Year</th>
+            <th>Head of State</th>
+        </tr>
+        <?php foreach ($results as $row): ?>
+            <tr>
+                <td><?= $row['name']; ?></td>
+                <td><?= $row['continent']; ?></td>
+                <td><?= $row['independence_year']; ?></td>
+                <td><?= $row['head_of_state']; ?></td>
+            </tr>
+        <?php endforeach; ?>
+    <?php endif; ?>
+</table>
